318bd8f60f8bb03b3792d8005151d1770b6b882d
Shimoni Kacheria <shimoni.kacharia@gmail.com>
2019-02-12 16:44:01 +0000

    Adding initial model and server with templates for project

---
+++ model.py
@@ -0,0 +1,95 @@
+"""Models and database functions for Meal Plate Tracker project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User of Meal Plate Tracker website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    f_name = db.Column(db.String(50), nullable=False)
+    l_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        """Provied helpful representation of class User when printed"""
+
+        return ('User user_id={} email={}'.format(self.user_id, self.email))
+
+
+class Meal(db.Model):
+    """Meal information for plate"""
+
+    __tablename__ = "meals"
+
+    meal_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    mealtime = db.Column(db.DateTime)
+    meal_name = db.Column(db.String(50)) #either breakfast, lunch or dinner from user dropdown
+    #2.0 features for calorie count:
+    # meal_description = db.Column(db.String(50), nullable=True) #nullable for now
+    # calories = db.Column(db.Numeric(8, 2), nullable=True) #nullable for now
+
+    users = db.relationship("User", backref="meals")
+    foodgroups = db.relationship("Foodgroup", secondary="meals_foodgroups", backref="meals")
+
+    def __repr__(self):
+        """Provide helpful representation of class Meal when printed"""
+
+        return ('Meal: id={} mealtime={}'.format(self.meal_id, self.mealtime))
+
+
+class Foodgroup(db.Model):
+    """Food group information for plate"""
+
+    __tablename__ = "foodgroups"
+
+    foodgroup_id = db.Column(db.Integer, primary_key=True)
+    foodgroup_name = db.Column(db.String(15), nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation of class Meal when printed"""
+
+        return ('Foodgroup: id={} name={}'.format(self.foodgroup_id, self.foodgroup_name))
+
+
+class Meal_Foodgroup(db.Model):
+    """Associatiion table between foodgroup and meal"""
+
+    __tablename__ = "meal_foodgroups"
+
+    meal_foodgroup_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    meal_id = db.Column(db.Integer, db.ForeignKey('meals.meal_id'))
+    foodgroup_id = db.Column(db.Integer, db.ForeignKey('foodgroups.foodgroup_id'))
+    plate_section_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        """Provide helpful representation of class Meal_Foodgroup(association table)"""
+
+        return ('meal-foodgroup-id: {} meal-id: {}'.format(self.meal_foodgroup_id, self.meal_id))
+""""""
+""""""
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///mealtracker'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+
+    connect_to_db(app)
+    print("Connected to DB.")
+    db.create_all()
---
+++ server.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, redirect, session, request, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from model import User, Meal, Foodgroup, Meal_Foodgroup
+
+app = Flask(__name__)
+
+app.secret_key = "1234"
+
+@app.route('/')
+def homepage():
+    """Homepage"""
+
+    return render_template('homepage.html')
+
+""""""
+
+@app.route('/log_in_check', methods=['POST'])
+def check_login():
+    """Check user log in information before rendering log_meal page and storing info in session"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    #Email and password query check if mateches
+    query = User.query.filter(User.email == email , User.password == password).first()
+
+    if query:
+        session['user_id'] = query.user_id
+        flash('You are successfully logged in')
+        return redirect('/log_meal')
+    else:
+        return redirect('/')
+
+""""""
+
+@app.route('/register')
+def register_user():
+    """Registration for user"""
+
+    return render_template('register.html')
+
+""""""
+
+@app.route('/thank_you', methods=['POST'])
+def thank_user():
+    """Thank user for registering"""
+
+    flash('Thank you for registering for your Meal Plate Tracker')
+    return redirect('/log_meal')
+
+""""""
+
+@app.route('/log_meal')
+def log_meal():
+    """Logging a meal for user"""
+
+    return render_template('/log_meal.html')
+
+@app.route
+
+""""""
+
+if __name__ == "__main__":
+
+    app.debug = True
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
